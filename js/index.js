08ef2ad12db6f8c0a3f952c3da7599d683569541
tushen121 <aizhifu121@qq.com>
2020-06-15 11:15:09 +0800

    本地版本功能全部完成，分类的增删改，网址数据的正删改

--- js/index.js
+++ js/index.js
@@ -76,13 +76,37 @@ var app = new Vue({
     data: {
         sortIndex:0,
         webDataIndex:0,
+        editSortIndex:0,//编辑分类的时候保存当前点击的时第几个分类。避免冲突
         message: 'Hello Vue!',
         editEnanle:false,// 显示或隐藏编辑、删除、添加图标、‘取消编辑’文字
         addSortCase:false,//添加分类弹出框
         editWebDataCase:false,//编辑选项弹出框
         addWebDataCase:false,//添加选项弹出框
-        sortDatas:sortData,//类别数据
-        AllwebDatas:webData,//所有类别的网址数据
+        editSortCase:false,//编辑分类弹出框
+        sortDatas: sortData,//类别数据
+        AllwebDatas: webData,//所有类别的网址数据
+    },
+    //页面加载时调用的函数
+    mounted:function(){
+
+        //如果在本地存有数据的话，就把本地数据给vue变量
+        if (localStorage.getItem("sortData")!=null && localStorage.getItem("webData")!=null) {
+            //检查存储的数据是否有误
+                try{
+                    this.sortDatas=JSON.parse(localStorage.getItem("sortData"))//读取本地数据赋值给vue变量
+                    this.AllwebDatas=JSON.parse(localStorage.getItem("webData"))
+                }catch (e) {
+                    this.sortDatas=sortData//类别数据
+                    this.AllwebDatas=webData//所有类别的网址数据
+                    alert("数据出错，已经恢复出厂设置");
+                }
+        }
+        //否则就把默认数据给vue变量
+        else{
+            this.sortDatas=sortData//类别数据
+            this.AllwebDatas=webData//所有类别的网址数据
+        }
+
     },
     methods: {
         // 显示或隐藏编辑、删除、添加图标、‘取消编辑’文字
@@ -93,7 +117,7 @@ var app = new Vue({
         {
             this.webDatas=this.AllwebDatas[index];
             this.sortIndex=index;
-            console.log(this.webDatas)
+           // console.log(this.webDatas)
         },
         searchEngine()
         {
@@ -185,6 +209,51 @@ var app = new Vue({
                 ]
             )
             this.addSortCase=false;//隐藏添加分类框
+        },
+        showEditSortCase(index){
+            this.editSortIndex=index;//标记当前要编辑哪个分类
+            $("#SortEditInput").val(this.sortDatas[index]);//分类名称放到input里
+            this.editSortCase=true;//编辑分类弹出框
+        },
+        hiddenEditSortCase()
+        {
+            this.editSortCase=false;//隐藏分类弹出框
+        },
+        removeSort(index){
+            this.sortDatas.splice(index,1);//删除分类
+            this.AllwebDatas.splice(index,1);//删除网址数据
+        },
+        editSort(){
+
+            this.sortDatas[this.editSortIndex]=$("#SortEditInput").val();
+            this.editSortCase=false;//隐藏分类弹出框
+        },
+        cancelEditClick()
+        {
+
+            this.togleEditEnable()// 显示或隐藏编辑、删除、添加图标、‘取消编辑’文字
+            //读取保存的数据，
+            try{
+                this.sortDatas=JSON.parse(localStorage.getItem("sortData"))//读取本地数据赋值给vue变量
+                this.AllwebDatas=JSON.parse(localStorage.getItem("webData"))
+            }catch (e) {
+                this.sortDatas=sortData//类别数据
+                this.AllwebDatas=webData//所有类别的网址数据
+                alert("数据出错，已经恢复出厂设置");
+            }
+        },
+        editClick()
+        {
+            this.togleEditEnable()// 显示或隐藏编辑、删除、添加图标、‘取消编辑’文字
+        },
+        saveClick()
+        {
+            //将变量数据保存到本地
+            localStorage.setItem("sortData",JSON.stringify(this.sortDatas))
+            localStorage.setItem("webData",JSON.stringify(this.AllwebDatas))
+
+            this.togleEditEnable()// 显示或隐藏编辑、删除、添加图标、‘取消编辑’文字
+
         }
 
 
